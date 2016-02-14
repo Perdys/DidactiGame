c51a458e6793d05852fc24328c413de9d14c6e52
Perdy <erperdy_90@hotmail.com>
2016-02-14 21:09:24 +0100

    empezamos

--- core/src/com/mygdx/psplbr/PsPlbr.java
+++ core/src/com/mygdx/psplbr/PsPlbr.java
@@ -1,27 +1,24 @@
 package com.mygdx.psplbr;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class PsPlbr extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("fondo.jpg");
+public class PsPlbr extends Game {
+
+	public void create() { setScreen(new MenuPrincipal(this)); }
+
+	public void render() { super.render(); }
+
+	public void resize(int width, int height) {
+		super.resize(width, height);
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void pause() {
+		super.pause();
+	}
+
+	public void resume() { super.resume(); }
+
+	public void dispose() {
+		super.dispose();
 	}
 }
