be03f57f860da9ab171e089bf294307206f0f6ea
Perdy <erperdy_90@hotmail.com>
2016-02-14 18:58:42 +0100

    Creacion
    creacion

---
+++ android/src/com/mygdx/psplbr/AndroidLauncher.java
@@ -0,0 +1,21 @@
+package com.mygdx.psplbr;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.psplbr.PsPlbr;
+import android.view.WindowManager;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		config.useGL20 = true;
+
+		initialize(new PsPlbr(), config);
+	}
+}
